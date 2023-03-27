241f4eff36162faf5464adaa312d05a7f1a73f4b
Maxence Grondin <202141803@edu.clg.qc.ca>
2023-03-26 23:37:03 -0400

    arcade push!!! ike!!!!!

--- Assets/Scripts/Arcade/TileController.cs
+++ Assets/Scripts/Arcade/TileController.cs
@@ -9,13 +9,13 @@ using Random = UnityEngine.Random;
 public class TileController : MonoBehaviour
 {
     private int startNumber = 6;
-    private int turns = 0;
-   
-    [SerializeField] private int tileSize = 5;
+    private int turns;
+
+    [SerializeField] private int tileSize = 30;
 
     [SerializeField] private List<GameObject> tileList;
     [SerializeField] private GameObject tileGenerator;
-    
+
 
     private void Start()
     {
@@ -28,47 +28,59 @@ public class TileController : MonoBehaviour
     public void SpawnTile()
     {
         int tileGenerated = Rnd();
-        Instantiate(tileList[tileGenerated], tileGenerator.transform.position, tileGenerator.transform.rotation);
+        //ApplyHeightChange(tileGenerated);
+        Instantiate(tileList[tileGenerated],  tileGenerator.transform.position,  tileGenerator.transform.rotation);
         tileGenerator.transform.rotation *= Quaternion.Euler(0, GetRotation(tileGenerated), 0);
         tileGenerator.transform.position += tileGenerator.transform.forward * tileSize;
     }
 
     private int GetRotation(int i)
     {
-        int deg = 0;
-        if (i == 1)
+        var deg = i switch
         {
-            deg = 270;
-        }
-        else if (i == 2)
-        {
-            deg = 90;
-        }
+            0 => 270,
+            1 => 90,
+            _ => 0
+        };
         return deg;
+    }
+
+    private void ApplyHeightChange(int tile)
+    {
+        switch (tile)
+        {
+            case 3:
+                tileGenerator.transform.Translate(0, 30, 0);
+                break;
+            case 4:
+                tileGenerator.transform.Translate(0, -30, 0);
+                break;
+        }
     }
 
     private int Rnd()
     {
-        int r = Random.Range(0, 5);
-        if (r==1)
+        var r = Random.Range(0, 10);
+        switch (r)
         {
-            turns++;
-            if (turns>1)
-            {
-                r = 2;
-                turns = 0;
-            }
-        }
-        else if (r == 2)
-        {
-            turns--;
-            if (turns<-1)
-            {
-                r = 1;
-                turns = 0;
-            }
-        }
+            case 0:
+                turns++;
+                if (turns > 1)
+                {
+                    r = 1;
+                    turns = 0;
+                }
 
+                break;
+            case 1:
+                turns--;
+                if (turns < -1)
+                {
+                    r = 0;
+                    turns = 0;
+                }
+                break;
+        }
         return r;
     }
 }
