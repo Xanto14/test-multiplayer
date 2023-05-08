92fd40518bfe17ccb04e5c34f8f85ca76f2deb85
Maxence Grondin <202141803@edu.clg.qc.ca>
2023-05-08 11:34:57 -0400

    push xepto

--- Assets/Scripts/OnlineRace/CheckpointCounter.cs
+++ Assets/Scripts/OnlineRace/CheckpointCounter.cs
@@ -10,9 +10,11 @@ public class CheckpointCounter : MonoBehaviour
     private LapCounter lapCounter;
     private float lapStartTime;
     private int checkpointIndex = 1;
-
+    private PhotonView photonView;
     private void Start()
     {
+        photonView = GetComponent<PhotonView>();
+        
         lapCounter = FindObjectOfType<LapCounter>();
         if (lapCounter == null)
         {
@@ -26,16 +28,26 @@ public class CheckpointCounter : MonoBehaviour
         {
             float lapTime = Time.time - lapStartTime;
             other.gameObject.SetActive(false);
-            lapCounter.checkpointList[checkpointIndex % lapCounter.checkpointList.Count].SetActive(true);
-            Player player = Photon.Pun.PhotonView.Get(this.gameObject).Owner;
+
+            // Only activate the checkpoint locally on the client that triggered it
+            photonView.RPC("ActivateCheckpoint", RpcTarget.AllBuffered, (checkpointIndex + 1) % lapCounter.checkpointList.Count);
+
+            Player player = PhotonView.Get(this.gameObject).Owner;
             if (checkpointIndex % lapCounter.checkpointList.Count == 0)
             {
                 lapCounter.PlayerFinishedLap(player, lapTime);
                 lapStartTime = Time.time;
                 Debug.LogError("Lap time for " + player.NickName + ": " + lapTime);
             }
-            
+        
             checkpointIndex++;
         }
     }
+    
+    [PunRPC]
+    private void ActivateCheckpoint(int index)
+    {
+        lapCounter.checkpointList[index].SetActive(true);
+    }
+
 }
